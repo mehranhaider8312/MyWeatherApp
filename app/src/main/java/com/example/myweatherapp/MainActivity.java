9c814f50d79ab74765650e9d83d426f6401e0881
Mehran Haider <168021778+mehranhaider8312@users.noreply.github.com>
2024-07-01 11:56:28 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -62,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        init();
+        
     }
     public void init()
     {
